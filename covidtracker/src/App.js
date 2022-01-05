a4263c701d05578e0091d54d1e67e21ddded3254
ganshika <ganshika@bridgetree.com>
2022-01-05 15:34:25 +0530

    covidtracker

---
+++ covidtracker/src/App.js
@@ -0,0 +1,16 @@
+ import React from 'react'
+ import Statewise from './components/statewiseData/statewise'; 
+ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+ 
+ const App = () => {
+   return (
+     <>
+       <Statewise/>
+     </>
+   )
+ }
+ 
+ 
+ 
+export default App
